326eb79240d23e3257a07c33764088fee1cae77e
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-12-18 16:20:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,20 @@ import Shop from './pages/Shop';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Cart from './pages/Cart';
+import ScrollToTop from './components/ScrollToTop';
+
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
     <CartProvider>
       <div>
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/cart" element={<Cart />} />
